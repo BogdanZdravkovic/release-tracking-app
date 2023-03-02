84679dbcadfd073dade794dd0af7a994b1490419
ZdravkovicB <bogdan.zdravkovic@bahag.com>
2023-03-02 16:09:11 +0100

    adding JUnit tests

--- src/main/java/com/bogdan/releasetracking/exception/ControllerExceptionHandler.java
+++ src/main/java/com/bogdan/releasetracking/exception/ControllerExceptionHandler.java
@@ -1,22 +1,16 @@
 package com.bogdan.releasetracking.exception;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
@@ -24,9 +18,8 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     private static final String BAD_REQUEST = "BAD_REQUEST";
 
     @Override
-    public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException exception,
-                                                               HttpHeaders headers, HttpStatus status, WebRequest request) {
-
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
         String errorDetails = "Unacceptable JSON " + exception.getMessage();
 
         if (exception.getCause() instanceof InvalidFormatException) {
@@ -40,22 +33,6 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
         errorResponse.setTitle(BAD_REQUEST);
         errorResponse.setDescription(errorDetails);
         return handleExceptionInternal(exception, errorResponse, headers, HttpStatus.BAD_REQUEST, request);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
-                                                                  HttpStatus status, WebRequest request) {
-        Map<String, List<String>> body = new HashMap<>();
-
-        List<String> errors = ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.toList());
-
-        body.put("errors", errors);
-
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
 }
